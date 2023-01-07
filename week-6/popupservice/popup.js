d8efcbb65040b31c7cdaa5466093fa413f4f1517
Dana Hafif <117688436+DanaHa-24@users.noreply.github.com>
2023-01-07 11:23:28 +0200

    Version 1.0

---
+++ week-6/popupservice/popup.js
@@ -0,0 +1,50 @@
+function PopupService() {
+  let currentPopup = null;
+
+  function open(popupBody, options) {
+    // If a popup is already open, close it
+    if (currentPopup) {
+      close();
+    }
+
+    // Create the popup element
+    const popupElement = document.createElement('div');
+    popupElement.innerHTML = popupBody;
+    popupElement.className = options.popupClassName || '';
+    document.body.appendChild(popupElement);
+
+    // Set the current popup to the new popup element
+    currentPopup = popupElement;
+
+    // Close the popup when clicked outside
+    if (options.isCloseByClickOutside) {
+      document.addEventListener('click', closeOnClickOutside);
+    }
+  }
+
+  function close() {
+    if (currentPopup) {
+      // Remove the popup element from the DOM
+      document.body.removeChild(currentPopup);
+
+      // Reset the current popup to null
+      currentPopup = null;
+
+      // Remove the click event listener
+      document.removeEventListener('click', closeOnClickOutside);
+    }
+  }
+
+  function closeOnClickOutside(event) {
+    if (!currentPopup.contains(event.target)) {
+      close();
+    }
+  }
+
+  return {
+    open,
+    close,
+  };
+}
+
+export default PopupService;
